82a15cfa6f44d34b9f1c0cbf362f84e22d76bbec
acalvo701 <93265280+acalvo701@users.noreply.github.com>
2023-01-20 16:58:53 +0100

    api

--- BackEnd/src/authServer.ts
+++ BackEnd/src/authServer.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express = require("express");
 import JsonWebTokenError = require("jsonwebtoken");
-
+const cors = require('cors');
 const app = express();
 
 const guardiaRoutes = require("./Routes/guardia.ts");
@@ -11,6 +11,7 @@ const tornRoutes = require("./Routes/torn.ts");
 const unitatRoutes = require("./Routes/unitat.ts");
 const categoriaRoutes = require("./Routes/categoria.ts");
 
+app.use(cors());
 app.use(express.json());
 app.use("/guardia", guardiaRoutes);
 app.use("/treballador", treballadorRoutes);
