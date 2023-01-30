cc2165df2e100ae2776c1b43de0f7b0f22dc4da9
ivandpf <idelpozo8@boscdelacoma.cat>
2023-01-30 18:39:37 +0100

    Add GuardiaModel Get Names Models

--- BackEnd/src/Routes/guardiamodel.ts
+++ BackEnd/src/Routes/guardiamodel.ts
@@ -4,10 +4,11 @@ import GuardiaModelController from "../Controllers/GuardiaModelController";
 const router = express.Router();
 
 router.get('/getEsquema',GuardiaModelController.getEsquema);
+router.get('/getNomsEsquemaByIdTreballador',GuardiaModelController.getNomsEsquemaByIdTreballador);
 
 router.post('/insertEsquemaRow',GuardiaModelController.insertEsquemaRow);
 router.post('/updateEsquemaRow',GuardiaModelController.updateEsquemaRow);
 router.post('/deleteEsquemaRow',GuardiaModelController.deleteEsquemaRow);
+router.post('/generarGuardiesEsquema',GuardiaModelController.generarGuardiesEsquema);
 
-router.post('/generarGuardiesEsquema',GuardiaModelController.generarGuardiesEsquema)
 module.exports = router;
